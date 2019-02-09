a88662f01de0db052e89f3e2ca3cdf5173cf1537
Cody Cornelison <codysmart@gmail.com>
2019-02-08 23:19:10 -0800

    Auth missing

--- firebase/authUI.php
+++ firebase/authUI.php
@@ -1,4 +1,4 @@
-<script src="../firebase/authUI.js"></script>
+<script src="authUI.js"></script>
 
 <!-- The surrounding HTML is left untouched by FirebaseUI.
      Your app may use that space for branding, controls and other customizations.-->
