29352f21f96f9b7d7fa32669fb8f3e0702231fc6
Cody Cornelison <codysmart@gmail.com>
2019-02-08 23:40:01 -0800

    Updated nav bar with W3 schools menu

--- web/header.php
+++ web/header.php
@@ -1,12 +1,21 @@
 </head>
 <body>
 <header>
-    <div id="header-title">
-        <h1>Play Tracker</h1>
-    </div>
+<!--    Took this code from W3 Schools
+https://www.w3schools.com/howto/howto_js_mobile_navbar.asp-->
 
-    <div class="link-rightSide">
-        <a href="index.php">Home</a>
-        <a href="assignments.php">Assignments</a>
+    <!-- Top Navigation Menu -->
+    <div class="topnav">
+        <h1>Play Tracker</h1>
+        <!-- Navigation links (hidden by default) -->
+        <div id="myLinks">
+            <a href="myPlays.php">View my Plays</a>
+            <a href="addPlay.php">Add a Play</a>
+            <a href="latestPlays.php">Recent Plays</a>
+        </div>
+        <!-- "Hamburger menu" / "Bar icon" to toggle the navigation links -->
+        <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+            <i class="fa fa-bars"></i>
+        </a>
     </div>
 </header>
--- web/index.php
+++ web/index.php
@@ -1,11 +1,9 @@
 <?php include 'head.php'; ?>
 <?php include'../firebase/firebaseInit.php'; ?>
 
-<title>ATG Tracker</title>
+<title>ATG Play Tracker</title>
 <?php include 'header.php'; ?>
 <div class="topMessage">
-    <h1>Welcome to Azure Tides Gaming play tracker.</h1>
-    <br>
     <h3>Please sign in with your Google account to continue.</h3>
 </div>
 <?php include 'authUI.php'; ?>
