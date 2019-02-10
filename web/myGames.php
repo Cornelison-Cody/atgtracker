1d367a10da081f423a853a5d08ded660fbb1807f
Cody Cornelison <codysmart@gmail.com>
2019-02-09 19:45:31 -0800

    adding pictures for games owned

--- web/myGames.php
+++ web/myGames.php
@@ -4,7 +4,9 @@
 <?php include 'modules/header.php'; ?>
 <script>
 </script>
-<div id="dataDump">
+<div>
+
+    <img src="media/Splendor.JPG">
     <?php
     foreach($db->query('SELECT * FROM games_owned INNER JOIN user_table ON games_owned.firebase_uid = user_table.firebase_uid INNER JOIN game ON games_owned.game_id = game.game_id WHERE user_table.firebase_uid =' . $_GET[uid]) as $row) {
     echo $row[game_name];
