149da5eb80892fbef36053740e177e0603a93f31
Cody Cornelison <codysmart@gmail.com>
2019-02-09 20:05:50 -0800

    Updating style on pictures.

--- web/myGames.php
+++ web/myGames.php
@@ -5,7 +5,6 @@
 <script>
 </script>
 <div class="flex-container">
-    <img src="media/Splendor.JPG">
     <?php
     foreach($db->query('SELECT * FROM games_owned INNER JOIN user_table ON games_owned.firebase_uid = user_table.firebase_uid INNER JOIN game ON games_owned.game_id = game.game_id WHERE user_table.firebase_uid =' . $_GET[uid]) as $row) {
         echo "<div class='post-container'>
