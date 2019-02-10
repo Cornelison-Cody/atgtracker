f0b9c1ce77937d5ba988bcb02860d38d3b9de896
Cody Cornelison <codysmart@gmail.com>
2019-02-09 21:25:03 -0800

    Added Recent Plays page.

--- web/latestPlays.php
+++ web/latestPlays.php
@@ -1,3 +1,24 @@
 <?php include 'modules/head.php'; ?>
-    <title>Add a new Play</title>
+<?php include_once'../db/dbconnect.php'; ?>
+<title>Recent Plays</title>
 <?php include 'modules/header.php'; ?>
+<script>
+</script>
+<div class="flex-container">
+    <?php
+    foreach($db->query('SELECT * FROM plays INNER JOIN game ON plays.game_id = game.game_id') as $row) {
+        echo "<div class='post-container'>
+                <h3>" . $row[game_name] . "</h3>
+                <img src='media/" . $row[game_name] . ".JPG'>
+                <p>PLAYERS: " . $row[players_text] .  "</p>
+                <p>WINNER: " . $row[winner_text] .  "</p>
+                <p>Score: " . $row[score_text] .  "</p>
+                <p>Notes: " . $row[notes_text] .  "</p>             
+               </div>";
+    };
+    ?>
+</div>
+
+
+
+
