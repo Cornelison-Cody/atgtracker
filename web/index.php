504df1ec22e1ea9f7cc5153deeed14cf23870bbd
Cody Cornelison <codysmart@gmail.com>
2019-02-23 14:10:15 -0800

    Updated settings for production.

--- web/index.php
+++ web/index.php
@@ -9,10 +9,6 @@
     <h3>Click the dropdown menu in the top right to select your action.</h3>
 </div>
 
-<?php
-    $db->query('INSERT INTO user_table (firebase_uid, display_name) SELECT \'' . $POST[game] . '\' WHERE NOT EXISTS (SELECT * FROM game WHERE game_name =\'' . $POST[game] . '\')');
-?>
-
 </body>
 </html>
 
